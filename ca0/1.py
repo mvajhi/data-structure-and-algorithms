8a0cd411aaf57815b9a035cfd27a7c4477e4d360
mahdi vajhi <mvajhimv@gmail.com>
2023-10-17 15:03:36 +0330

    Fix ca0 1.py

--- ca0/1.py
+++ ca0/1.py
@@ -8,26 +8,17 @@ def calculate(txt: str) -> int:
     len_txt = len(txt)
 
     for i in range(len_txt):
-        counter_per_char = {i : 0 for i in ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]}
+        list_of_odd_word = []
         for j in range(i + 1, len_txt + 1):
-            counter_per_char[txt[j - 1]] += 1
-            if is_ok(counter_per_char):
+            if txt[j-1] in list_of_odd_word:
+                list_of_odd_word.remove(txt[j-1])
+            else:
+                list_of_odd_word.append(txt[j-1])
+
+            if len(list_of_odd_word) <= 1:
                 counter += 1
     
-    return counter
-            
-def is_ok(counter_per_char : dict) -> bool:
-    odd_counter = 0
-    
-    for i in counter_per_char.values():
-        if i % 2 == 1:
-            odd_counter += 1
-    
-    if odd_counter <= 1:
-        return True
-    else:
-        return False
-            
+    return counter 
 
 if __name__ == "__main__":
     main()
