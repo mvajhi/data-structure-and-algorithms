e41aff759a1e546b8ce77de92105989395797e00
mahdi vajhi <mvajhimv@gmail.com>
2023-10-23 11:46:24 +0330

    Write 1.py

--- ca1/1.py
+++ ca1/1.py
@@ -1,24 +1,39 @@
-def main():
-    input_str = input()
-    count = calculate(input_str)
-    print(count)
-
-def calculate(txt: str) -> int:
-    counter = 0
-    len_txt = len(txt)
-
-    for i in range(len_txt):
-        list_of_odd_word = []
-        for j in range(i + 1, len_txt + 1):
-            if txt[j-1] in list_of_odd_word:
-                list_of_odd_word.remove(txt[j-1])
-            else:
-                list_of_odd_word.append(txt[j-1])
-
-            if len(list_of_odd_word) <= 1:
-                counter += 1
+def create_new_bitmap(bit_map, char):
+    bit_place = ord(char) - ord("a")
     
-    return counter 
+    bit_map[bit_place] = (bit_map[bit_place] + 1) % 2
+    
+    return bit_map
 
-if __name__ == "__main__":
-    main()
+
+txt = input()
+
+result = 0
+odd_bitmap_list = [0 for i in range(10)]
+my_str = ""
+ans = dict()
+
+for char in txt:
+    my_str += char
+    odd_bitmap_list = create_new_bitmap(odd_bitmap_list, char)
+    odd_bitmap = tuple(odd_bitmap_list)
+    
+
+    if odd_bitmap.count(1) <= 1:
+        result += 1
+    
+    if odd_bitmap in ans.keys():
+        result += ans[odd_bitmap]
+        ans[odd_bitmap] += 1
+    else:
+        ans[odd_bitmap] = 1
+
+    for i in range(len(odd_bitmap)):
+        tmp = list(odd_bitmap)
+        tmp[i] = (tmp[i] + 1) % 2
+        tmp = tuple(tmp)
+        
+        if tmp in ans.keys():
+            result += ans[tmp]
+
+print(result)
